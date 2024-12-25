e23ffe6b630bc63cae842aa5590430151720c2a0
razrabot4ik123 <IIIRaZRaBoT4IKIII@yandex.ru>
2024-12-25 22:00:41 +0700

    fix to cocaSecond

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   root: './src',
-  base: '/coca/',
+  base: '/cocaSecond/',
   server: {
     port: 3000,
     open: true,
